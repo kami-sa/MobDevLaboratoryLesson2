82c3f21c6a0282fc07108646d0f91face28fa230
kami-sa <kamisa1999@yandex.ru>
2019-10-19 10:36:05 +0300

    Initial commit

---
+++ multiactivity/src/main/java/ru/mirea/saidova/multiactivity/MainActivity.java
@@ -0,0 +1,21 @@
+package ru.mirea.saidova.multiactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickNewActivity(View view) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        this.startActivity(intent);
+    }
+}
