dda1e6342451bf355f63f6724e46152b8b367224
kami-sa <kamisa1999@yandex.ru>
2019-10-20 17:13:12 +0300

    Add Dialog Window

---
+++ dialogapp/src/main/java/ru/mirea/saidova/dialogapp/MainActivityDialog.java
@@ -0,0 +1,33 @@
+package ru.mirea.saidova.dialogapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivityDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_dialog);
+    }
+    public void onClickShowDialog(View view) {
+        MyAlertDialogFragment dialogFragment = new MyAlertDialogFragment ();
+        dialogFragment.show(getSupportFragmentManager(), "dialog");
+    }
+
+    public void onOkClicked() {
+        Toast.makeText(getApplicationContext(), "Вы выбрали кнопку \"Иду дальше\"!",
+                Toast.LENGTH_LONG).show();
+    }
+    public void onCancelClicked() {
+        Toast.makeText(getApplicationContext(), "Вы выбрали кнопку \"Нет\"!",
+                Toast.LENGTH_LONG).show();
+    }
+    public void onNeutralClicked() {
+        Toast.makeText(getApplicationContext(), "Вы выбрали кнопку \"На паузе\"!",
+                Toast.LENGTH_LONG).show();
+    }
+}
