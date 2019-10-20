33c3911e68b9492df6a708c2c1b450099ec5fd18
kami-sa <kamisa1999@yandex.ru>
2019-10-20 15:32:41 +0300

    Add Intent Filter which help us open WebBrowser

---
+++ intentfilter/src/main/java/ru/mirea/saidova/intentfilter/MainActivityFilter.java
@@ -0,0 +1,24 @@
+package ru.mirea.saidova.intentfilter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivityFilter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_filter);
+        Uri address = Uri.parse("https://www.mirea.ru/");
+        Intent openLinkIntent = new Intent(Intent.ACTION_VIEW, address);
+        if (openLinkIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(openLinkIntent);
+         } else {
+             Log.d("Intent", "Проблема с намерением!");
+            }
+    }
+}
